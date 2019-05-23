bb40bf4cdd33ac15225ee0f7772b445afad0e096
Gabriel Cruvinel <cruvinel.gabriel.gc@gmail.com>
2019-05-23 13:03:54 -0300

    -

---
+++ sistema_escola/apps/pessoa/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Pessoa(models.Model0):
+    id_pessoa = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100, help_text='Nome', blank=False)
+    cpf = models.CharField(max_length=15, help_text='CPF', blank=False)
+    rg = models.CharField(max_length=10, help_text='RG', blank=True)
+    data_nascimento = models.DateField()
+    
